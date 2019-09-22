89d5cc58943b5041a94160a5a52fcbaaa3ec6aca
Hinoga <oscargallego27@gmail.com>
2019-09-21 19:08:59 -0500

    Landing page restructuringe

---
+++ script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById('menu')
+let links = document.getElementById('links')
+let flag = false
+if (btn) {
+  btn.addEventListener('click', () => {
+    if (flag === false) {
+      links.className = 'links two'
+      flag = true
+    } else {
+      links.classList.remove('two')
+      links.className = 'links one'
+      flag = false
+    }
+  })
+}
